dcbff50320fd4f9a8dc171ae69825c583796dd04
Нурия <nhaliullina5@gmail.com>
2024-12-11 20:52:04 +0300

    first

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+from false_math import divide as f
+from true_math import divide as t
+
+
+result1 = f(89, 4)
+result2 = f(6, 0)
+result3 = t(56, 8)
+result4 = t(90, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
